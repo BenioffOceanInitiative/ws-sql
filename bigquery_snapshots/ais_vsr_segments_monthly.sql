ca6ef517307def8ff84556f2667c7a51f93a5fb7
Paloma Cartwright <palomacartwright@gmail.com>
2022-04-29 14:14:28 -0700

    added the readme and the sql queries for all 15 scheduled backup queries

---
+++ bigquery_snapshots/ais_vsr_segments_monthly.sql
@@ -0,0 +1,11 @@
+-- Information on how to run this query in teh cloud console can be found in the snapshot.md document.
+
+DECLARE snapshot_name STRING; 
+DECLARE expiration TIMESTAMP; 
+DECLARE query STRING; 
+SET expiration = DATE_ADD(@run_time, INTERVAL 366 DAY); 
+SET snapshot_name = CONCAT("whalesafe_v3_backup.ais_vsr_segments_monthly_", 
+    FORMAT_DATETIME("%Y%m%d", @run_date)); SET query = CONCAT("CREATE SNAPSHOT TABLE ", 
+    snapshot_name, "CLONE benioff-ocean-initiative.whalesafe_v3.ais_vsr_segments 
+    OPTIONS(expiration_timestamp=TIMESTAMP \"", expiration, "\");"); 
+EXECUTE IMMEDIATE query;
